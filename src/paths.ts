a65db69bd87e470704d13b1fd6490408336be1aa
George Song <george@55minutes.com>
2015-10-28 09:29:17 -0700

    Update gulp paths

--- src/paths.ts
+++ src/paths.ts
@@ -11,7 +11,11 @@ export const BUILD_ALL = path.join(BUILD_DIR, "**/*");
 export const SRC_ALL = path.join(SRC_DIR, "**/*");
 export const SRC_HTML = path.join(SRC_DIR, "**/*.html");
 export const SRC_INDEX_HTML = path.join(SRC_DIR, "index.html");
-export const SRC_SCRIPT = path.join(SRC_DIR, "**/*.js");
+export const SRC_SCRIPT = [
+  `!${path.join(SRC_DIR, "{jspm_packages,typings}/**/*")}`,
+  path.join(SRC_DIR, "**/*.ts")
+];
+export const SRC_SCRIPT_ALL = path.join(SRC_DIR, "**/*.{js,ts}");
 export const SRC_STYLE = path.join(SRC_DIR, "**/*.scss");
 
 export const TMP_IMAGE = path.join(TMP_DIR, "**/*.+(png|jpg|svg)");
@@ -21,4 +25,4 @@ const INDEX_SCRIPT_BASE = "index";
 export const INDEX_SCRIPT = `${INDEX_SCRIPT_BASE}.js`;
 export const BUILD_INDEX_JS = path.join(BUILD_DIR, INDEX_SCRIPT);
 export const TMP_INDEX_JS = path.join(TMP_DIR, INDEX_SCRIPT_BASE);
-export const SETTINGS = "config/settings.js";
+export const SETTINGS = "config/settings.ts";
